f96fe62b9a03b48d945bf917f0cd1782b8522c3d
Sam Bentley <sam.bentley@contino.io>
2022-04-11 16:01:32 +1000

    add RG

--- infrastructure/resourceGroup.tf
+++ infrastructure/resourceGroup.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "this" {
   name     = "weather-app"
   location = "Australia East"
 }
+
+resource "azurerm_resource_group" "two" {
+  name     = "weather-two"
+  location = "Australia East"
+}
