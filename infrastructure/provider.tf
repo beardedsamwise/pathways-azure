1e2fb65a286ed47de54f04ff7e3c5a1b233b26f1
Sam Bentley <sam.bentley@contino.io>
2022-04-11 15:47:36 +1000

    updates

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstatebearded"
+    container_name       = "tfstate"
+    key                  = "weatherapp.tfstate"
+  }
 }
 
 provider "azurerm" {
